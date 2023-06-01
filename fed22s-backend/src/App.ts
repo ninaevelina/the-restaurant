fc55914896df5b824e96276b5bb6c6846ad96452
mira <miravitsas1@gmail.com>
2023-06-01 10:44:45 +0200

    (build): server setup

---
+++ fed22s-backend/src/App.ts
@@ -0,0 +1,44 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import "express-async-errors";
+import mongoose from "mongoose";
+import bookingRoutes from "./routes/bookingRoutes";
+import path from "path";
+import { json } from "express";
+import { env } from "process";
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(`processing ${req.method} processing ${req.method}`);
+  console.log(req.path);
+  next();
+});
+
+app.use("/api/v1/booking", bookingRoutes);
+
+app.use((req, res) => {
+  const isApiPath = req.path.startsWith("/api/");
+
+  if (isApiPath) return res.sendStatus(404);
+
+  return;
+});
+
+const port = process.env.PORT || 5000;
+async function run() {
+  try {
+    mongoose.set("strictQuery", false);
+    const conn = await mongoose.connect(process.env.MONGODB_URI || "");
+
+    app.listen(port, () => {
+      console.log(`server running on http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+run();
