b5ef30304ccb783b9a53cec6e822785b326d17d4
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2023-06-01 12:58:34 +0200

    (feat): add branch for test-environment

--- fed22s-backend/src/controllers/bookingController.ts
+++ fed22s-backend/src/controllers/bookingController.ts
@@ -1,9 +1,9 @@
 import { RequestHandler } from "express";
 
 export const createBooking: RequestHandler = (req, res, next) => {
-  res.json("test");
+  res.json("test msg");
 };
 
 export const deleteBooking: RequestHandler = (req, res, next) => {
-  res.json("test");
+  res.json("test msg");
 };
