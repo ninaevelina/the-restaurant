68e9d84461f5b9e39dda60ca3725783f05cdcb21
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2023-06-05 11:46:55 +0200

    (feat): create bookingRoutes
    - fix error for: deletebooking + get all bookings

--- fed22s-backend/src/controllers/bookingController.ts
+++ fed22s-backend/src/controllers/bookingController.ts
@@ -1,9 +1,62 @@
 import { RequestHandler } from "express";
+import { Booking } from "../models/Booking";
 
-export const createBooking: RequestHandler = (req, res, next) => {
-  res.json("test msg");
+export const createBooking: RequestHandler = async (req, res, next) => {
+  const { people, date, sitting, guest } = req.body;
+  const newBooking = await Booking.create({
+    people: people,
+    date: date,
+    sitting: sitting,
+    guest: guest,
+  });
+  return (
+    res
+      // .setHeader(
+      //   "location",
+      //   `http://localhost:${process.env.PORT}/api/v1/booking/${newBooking._id}`
+      // )
+      .status(201)
+      .json(newBooking)
+  );
 };
 
-export const deleteBooking: RequestHandler = (req, res, next) => {
-  res.json("test msg");
+export const deleteBooking: RequestHandler = async (req, res, next) => {
+  const bookingId = req.params.id;
+
+  await Booking.findByIdAndDelete(bookingId);
+  console.log(bookingId);
+  //const booking = await Booking.findById({ _id: bookingId });
+  //await booking.
+  //const bookingToDelete = await booking?.deleteOne(bookingId.)
+
+  return res.send("deleted").status(204);
+  //return res.status(204).json(booking);
+  /*
+  const bookingID = req.params.bookingID;
+
+  const bookingToDelete = await Booking.findById(bookingID);
+
+  if (!bookingID) return res.sendStatus(404);
+
+  const response = await bookingToDelete?.deleteOne();
+
+  return res.send("deleted").status(204);*/
+  /*
+  const bookingID = req.params.id;
+
+  const bookingToDelete = await Booking.findById(bookingID);
+
+  if (!bookingToDelete) {
+    return res.sendStatus(404);
+  }
+
+  const response = await bookingToDelete.deleteOne();
+
+  return res.send("deleted").status(204);*/
+};
+
+export const getAllBookings: RequestHandler = async (req, res, next) => {
+  const bookings = await Booking.find();
+  const totalBookings = await Booking.countDocuments();
+  return res.json(bookings);
 };
--- fed22s-backend/src/routes/bookingRoutes.ts
+++ fed22s-backend/src/routes/bookingRoutes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { createBooking, deleteBooking } from "../controllers/bookingController";
+import {
+  createBooking,
+  deleteBooking,
+  getAllBookings,
+} from "../controllers/bookingController";
 
 const router: Router = Router();
 
 router.post("/", createBooking);
 router.delete("/:id", deleteBooking);
+router.get("/", getAllBookings);
 
 export default router;
