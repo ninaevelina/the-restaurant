1b3fe7d0ce7364b113458ad6c3e53e3a7ad6559d
mira <miravitsas1@gmail.com>
2023-06-05 15:27:39 +0200

    (build) implemented sitting option buttons

--- fed22s-therestaurant/src/components/pages/Booking.tsx
+++ fed22s-therestaurant/src/components/pages/Booking.tsx
@@ -2,9 +2,11 @@ import { useState } from "react";
 import { CalendarReact } from "../CalendarReact";
 import { Form } from "../Form";
 import { Navbar } from "../Navbar";
+import { SittingOption } from "../SittingOption";
 
 export const Booking = () => {
-  const [show, setShow] = useState(true);
+  const [showForm, setShowForm] = useState(true); //ska vara false
+  const [showSittingButton, setShowSittingButton] = useState(true); //ska vara false
 
   return (
     <>
@@ -12,7 +14,8 @@ export const Booking = () => {
       <CalendarReact></CalendarReact>
       {/* // i Calender här ska vi göra en onclick som gör att när man väljer datum
       blir show true */}
-      {show && <Form></Form>}
+      {showSittingButton && <SittingOption></SittingOption>}
+      {showForm && <Form></Form>}
     </>
   );
 };
