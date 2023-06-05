2fb55d07be375d741657997273720ee0c8e4672e
mira <miravitsas1@gmail.com>
2023-06-05 13:51:45 +0200

    (feat): name form

--- fed22s-therestaurant/src/components/pages/Booking.tsx
+++ fed22s-therestaurant/src/components/pages/Booking.tsx
@@ -1,15 +1,18 @@
-
+import { useState } from "react";
 import { CalendarReact } from "../CalendarReact";
-
+import { Form } from "../Form";
 import { Navbar } from "../Navbar";
 
 export const Booking = () => {
+  const [show, setShow] = useState(true);
+
   return (
     <>
       <p>Booking</p>
-
       <CalendarReact></CalendarReact>
-
+      {/* // i Calender här ska vi göra en onclick som gör att när man väljer datum
+      blir show true */}
+      {show && <Form></Form>}
     </>
   );
 };
