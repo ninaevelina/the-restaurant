0e0d195a1901906b5bd3bea23774cae10d67d196
mira <miravitsas1@gmail.com>
2023-06-06 10:02:15 +0200

    (feat): number of guests option

--- fed22s-therestaurant/src/components/pages/Booking.tsx
+++ fed22s-therestaurant/src/components/pages/Booking.tsx
@@ -4,6 +4,8 @@ import { Form } from "../Form";
 import { Navbar } from "../Navbar";
 import { SittingOption } from "../SittingOption";
 
+import { GuestNumbers } from "../GuestNumbers";
+
 export const Booking = () => {
   const [showForm, setShowForm] = useState(true); //ska vara false
   const [showSittingButton, setShowSittingButton] = useState(true); //ska vara false
@@ -14,6 +16,7 @@ export const Booking = () => {
       <CalendarReact></CalendarReact>
       {/* // i Calender här ska vi göra en onclick som gör att när man väljer datum
       blir show true */}
+      <GuestNumbers></GuestNumbers>
       {showSittingButton && <SittingOption></SittingOption>}
       {showForm && <Form></Form>}
     </>
---
+++ fed22s-therestaurant/src/components/styled/GuestNumbersStyled.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const GuestNumbersStyled = styled.div`
+  width: 50px;
+  height: 50px;
+  border: 1px solid black;
+`;
