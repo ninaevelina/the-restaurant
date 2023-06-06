e4b9aea1e7da7d73ef95bd5477485104519cff0c
mira <miravitsas1@gmail.com>
2023-06-06 10:47:13 +0200

    (feat) added currentbooking state and context

--- fed22s-therestaurant/src/App.tsx
+++ fed22s-therestaurant/src/App.tsx
@@ -1,13 +1,30 @@
 import "./App.scss";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Router";
-import { useEffect, useReducer } from "react";
-import { BookingContext } from "./contexts/BookingContext";
+import { useEffect, useReducer, useState } from "react";
+import {
+  BookingContext,
+  CurrentBookingContext,
+} from "./contexts/BookingContext";
 import { BookingDispatchContext } from "./contexts/BookingDispatchContext";
 import { BookingReducer } from "./reducers/BookingReducer";
+import { IBooking } from "./models/IBooking";
 
 function App() {
   const [bookings, dispatch] = useReducer(BookingReducer, []);
+  const [currentBooking, setCurrentBooking] = useState<IBooking>({
+    _id: 0,
+    people: 0,
+    date: "",
+    sitting: "",
+    tables: [],
+    guest: {
+      name: "",
+      lastname: "",
+      email: "",
+      phone: 0,
+    },
+  });
 
   useEffect(() => {
     //Hämta datan här och lägga in i det vår bookings variabel via dispatch.
@@ -16,9 +33,11 @@ function App() {
   return (
     <>
       <BookingContext.Provider value={bookings}>
-        <BookingDispatchContext.Provider value={dispatch}>
-          <RouterProvider router={router} />
-        </BookingDispatchContext.Provider>
+        <CurrentBookingContext.Provider value={currentBooking}>
+          <BookingDispatchContext.Provider value={dispatch}>
+            <RouterProvider router={router} />
+          </BookingDispatchContext.Provider>
+        </CurrentBookingContext.Provider>
       </BookingContext.Provider>
     </>
   );
