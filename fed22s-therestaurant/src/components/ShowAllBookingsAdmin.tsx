adf8b6fcf1adcaa8695a1c0cebd636ad57783f8e
Jennie <forselljennie@gmail.com>
2023-06-08 14:38:48 +0200

    (feat): moved useReducer to Admin.tsx and present data in ShowallBookingsAdmin.tsx.

--- fed22s-therestaurant/src/components/ShowAllBookingsAdmin.tsx
+++ fed22s-therestaurant/src/components/ShowAllBookingsAdmin.tsx
@@ -1,5 +1,14 @@
+import { useContext, useEffect, useReducer } from "react";
+import { ActionType, BookingsReducer } from "../reducers/BookingsReducer";
+import { getAllBookings } from "../services/restaurantApi";
+import { BookingsContext } from "../contexts/BookingContext";
+import { BookingDispatchContext } from "../contexts/BookingDispatchContext";
+
 export const ShowAllBookingsAdmin = () => {
   //Hämta datan i contextet och loopa ut för varje html-tag
+  const bookings = useContext(BookingsContext);
+  const dispatch = useContext(BookingDispatchContext);
+  console.log(bookings);
 
   //när klicket gjorts ska bokningen vara borta från domen och databasen.
   const deleteBooking = () => {
@@ -13,19 +22,38 @@ export const ShowAllBookingsAdmin = () => {
     return;
   };
 
-  const list: [] = [];
-  const showList = list.map((list) => {
-    <div>
-      <p>date</p>
-      <p>guests</p>
-      <p>Firstname</p>
-      <p>Lastname</p>
-      <p>Phone</p>
-      <p>email</p>
-      <button onClick={deleteBooking}>delete booking</button>
-      <button onClick={updateBooking}>update booking</button>
-    </div>;
+  const allBookings = bookings.map((b) => {
+    return (
+      <div>
+        <p>date: {b.date}</p>
+        <p>guests: {b.people}</p>
+        <p>Firstname{b.guest.name}</p>
+        <p>Lastname{b.guest.lastname}</p>
+        <p>Phone{b.guest.phone}</p>
+        <p>email{b.guest.email}</p>
+        <button onClick={deleteBooking}>delete booking</button>
+        <button onClick={updateBooking}>update booking</button>
+      </div>
+    );
   });
 
-  return <>{showList}</>;
+  return (
+    // <div>
+    //   {state.map((b) => {
+    //     return (
+    //       <div key={b._id}>
+    //         <p>date: {b.date}</p>
+    //         <p>guests: {b.people}</p>
+    //         {/* <p>Firstname: {b.guest.name}</p> */}
+    //         <p>Lastname: {b.guest.lastname}</p>
+    //         <p>Phone: {b.guest.phone}</p>
+    //         <p>Email: {b.guest.email}</p>
+    //         <button onClick={deleteBooking}>delete booking</button>
+    //         <button onClick={updateBooking}>update booking</button>
+    //       </div>
+    //     );
+    //   })}
+    // </div>
+    <>{allBookings}</>
+  );
 };
--- fed22s-therestaurant/src/components/pages/Admin.tsx
+++ fed22s-therestaurant/src/components/pages/Admin.tsx
@@ -1,9 +1,36 @@
+import { useEffect, useReducer } from "react";
+import { ActionType, BookingsReducer } from "../../reducers/BookingsReducer";
+import { getAllBookings } from "../../services/restaurantApi";
 import { ShowAllBookingsAdmin } from "../ShowAllBookingsAdmin";
+import { BookingsContext } from "../../contexts/BookingContext";
+import { BookingDispatchContext } from "../../contexts/BookingDispatchContext";
 
 export const Admin = () => {
+  const [bookings, dispatch] = useReducer(BookingsReducer, []);
+  console.log(bookings);
+
+  useEffect(() => {
+    const getData = async () => {
+      const getAllDataFromApi = await getAllBookings();
+      dispatch({
+        type: ActionType.GOTALLBOOKINGS,
+        payload: JSON.stringify(getAllDataFromApi),
+      });
+      console.log(getAllDataFromApi);
+    };
+
+    getData();
+  }, [dispatch]);
+
+  console.log(bookings);
+
   return (
     <>
-      <ShowAllBookingsAdmin></ShowAllBookingsAdmin>
+      <BookingsContext.Provider value={bookings}>
+        <BookingDispatchContext.Provider value={dispatch}>
+          <ShowAllBookingsAdmin></ShowAllBookingsAdmin>
+        </BookingDispatchContext.Provider>
+      </BookingsContext.Provider>
     </>
   );
 };
