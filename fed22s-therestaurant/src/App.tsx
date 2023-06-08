9505f1ac156d858f33d9ddd1f7386f8337f64752
mira <miravitsas1@gmail.com>
2023-06-08 14:50:56 +0200

    (fix) moved context to booking comp

--- fed22s-therestaurant/src/App.tsx
+++ fed22s-therestaurant/src/App.tsx
@@ -1,14 +1,8 @@
 import "./App.scss";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Router";
-import { useReducer, useState } from "react";
-import {
-  CurrentBookingContext,
-  IBookingContext,
-} from "./contexts/BookingContext";
+import { useReducer } from "react";
 import { BookingsReducer } from "./reducers/BookingsReducer";
-import { IGuest } from "./models/IGuest";
-import { createNewBooking } from "./services/restaurantApi";
 
 function App() {
   const [bookings, dispatch] = useReducer(BookingsReducer, [{}]);
@@ -27,79 +21,10 @@ function App() {
   //     },
   //   },
   // ]);
-  const [currentBooking, setCurrentBooking] = useState<IBookingContext>({
-    booking: {
-      _id: 0,
-      people: 0,
-      date: "",
-      sitting: "",
-      tables: [],
-      guest: {
-        name: "",
-        lastname: "",
-        email: "",
-        phone: 0,
-      },
-    },
-
-    addBooking: () => {
-      return;
-    },
-    updatePeople: (numberOfGuest: number) => {
-      return;
-    },
-    updateSeating: (seatingTime: string) => {
-      return;
-    },
-    updateForm: (guestInfo: IGuest) => {
-      return;
-    },
-    updateDate: (chosenData: any) => {
-      return;
-    },
-  });
-
-  currentBooking.addBooking = async () => {
-    console.log(currentBooking.booking);
-    try {
-      let result = await createNewBooking(currentBooking.booking);
-      console.log(result);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  currentBooking.updateDate = (chosenDate: any) =>
-    setCurrentBooking({
-      ...currentBooking,
-      booking: { ...currentBooking.booking, date: chosenDate },
-    });
-
-  currentBooking.updatePeople = (numberOfGuest: number) => {
-    setCurrentBooking({
-      ...currentBooking,
-      booking: { ...currentBooking.booking, people: numberOfGuest },
-    });
-  };
-
-  currentBooking.updateSeating = (seatingTime: string) => {
-    setCurrentBooking({
-      ...currentBooking,
-      booking: { ...currentBooking.booking, sitting: seatingTime },
-    });
-  };
-  currentBooking.updateForm = (guestInfo: IGuest) => {
-    setCurrentBooking({
-      ...currentBooking,
-      booking: { ...currentBooking.booking, guest: guestInfo },
-    });
-  };
 
   return (
     <>
-      <CurrentBookingContext.Provider value={currentBooking}>
-        <RouterProvider router={router} />
-      </CurrentBookingContext.Provider>
+      <RouterProvider router={router} />
     </>
   );
 }
