5ca3b56f0d59ac813b961aa0d5064469a6a60e06
mira <miravitsas1@gmail.com>
2023-06-08 14:46:18 +0200

    (fix) clean up code

--- fed22s-therestaurant/src/App.tsx
+++ fed22s-therestaurant/src/App.tsx
@@ -1,35 +1,32 @@
 import "./App.scss";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Router";
-import { useEffect, useReducer, useState } from "react";
+import { useReducer, useState } from "react";
 import {
   CurrentBookingContext,
   IBookingContext,
 } from "./contexts/BookingContext";
-import { BookingDispatchContext } from "./contexts/BookingDispatchContext";
 import { BookingsReducer } from "./reducers/BookingsReducer";
-import { IBooking } from "./models/IBooking";
-import axios from "axios";
 import { IGuest } from "./models/IGuest";
 import { createNewBooking } from "./services/restaurantApi";
 
 function App() {
   const [bookings, dispatch] = useReducer(BookingsReducer, [{}]);
-  const [allBookings, setAllBookings] = useState<IBooking[]>([
-    {
-      _id: 0,
-      people: 0,
-      date: "",
-      sitting: "",
-      tables: [],
-      guest: {
-        name: "",
-        lastname: "",
-        email: "",
-        phone: 0,
-      },
-    },
-  ]);
+  // const [allBookings, setAllBookings] = useState<IBooking[]>([
+  //   {
+  //     _id: 0,
+  //     people: 0,
+  //     date: "",
+  //     sitting: "",
+  //     tables: [],
+  //     guest: {
+  //       name: "",
+  //       lastname: "",
+  //       email: "",
+  //       phone: 0,
+  //     },
+  //   },
+  // ]);
   const [currentBooking, setCurrentBooking] = useState<IBookingContext>({
     booking: {
       _id: 0,
@@ -98,28 +95,11 @@ function App() {
     });
   };
 
-  //detta ska göras i reducerdokumentet
-  useEffect(() => {
-    //hur lägger vi in datan i bookings i reducern?
-    axios
-      .get<IBooking[]>("http://localhost:4000/api/v1/booking")
-      .then((response) => {
-        setAllBookings(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
-
   return (
     <>
-      {/* <BookingContext.Provider value={bookings}> */}
       <CurrentBookingContext.Provider value={currentBooking}>
-        <BookingDispatchContext.Provider value={dispatch}>
-          <RouterProvider router={router} />
-        </BookingDispatchContext.Provider>
+        <RouterProvider router={router} />
       </CurrentBookingContext.Provider>
-      {/* </BookingContext.Provider> */}
     </>
   );
 }
