aa7f1d75cf39a6ef1b2b672355a504a73f1c6224
Jennie <forselljennie@gmail.com>
2023-06-08 15:41:42 +0200

    (feat): delete a booking in dom

--- fed22s-therestaurant/src/App.tsx
+++ fed22s-therestaurant/src/App.tsx
@@ -1,11 +1,8 @@
 import "./App.scss";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Router";
-import { useReducer } from "react";
-import { BookingsReducer } from "./reducers/BookingsReducer";
 
 function App() {
-  const [bookings, dispatch] = useReducer(BookingsReducer, [{}]);
   // const [allBookings, setAllBookings] = useState<IBooking[]>([
   //   {
   //     _id: 0,
--- fed22s-therestaurant/src/components/ShowAllBookingsAdmin.tsx
+++ fed22s-therestaurant/src/components/ShowAllBookingsAdmin.tsx
@@ -1,26 +1,16 @@
-import { useContext, useEffect, useReducer } from "react";
-import { ActionType, BookingsReducer } from "../reducers/BookingsReducer";
-import { getAllBookings } from "../services/restaurantApi";
+import { useContext, useState } from "react";
 import { BookingsContext } from "../contexts/BookingContext";
 import { BookingDispatchContext } from "../contexts/BookingDispatchContext";
+import { ActionType } from "../reducers/BookingsReducer";
 
 export const ShowAllBookingsAdmin = () => {
   //Hämta datan i contextet och loopa ut för varje html-tag
   const bookings = useContext(BookingsContext);
   const dispatch = useContext(BookingDispatchContext);
   console.log(bookings);
+  const [showUpdate, setShowUpdate] = useState(false);
 
-  //när klicket gjorts ska bokningen vara borta från domen och databasen.
-  const deleteBooking = () => {
-    // kopplar på vår reducer delete här
-    return;
-  };
-
-  //när klicket gjorts så ska man kunna förändra
-  const updateBooking = () => {
-    //kopplar på vår reducer update här
-    return;
-  };
+  const updatebooking = () => {};
 
   const allBookings = bookings.map((b) => {
     return (
@@ -31,29 +21,29 @@ export const ShowAllBookingsAdmin = () => {
         <p>Lastname{b.guest.lastname}</p>
         <p>Phone{b.guest.phone}</p>
         <p>email{b.guest.email}</p>
-        <button onClick={deleteBooking}>delete booking</button>
-        <button onClick={updateBooking}>update booking</button>
+        <button
+          onClick={() => {
+            dispatch({
+              type: ActionType.DELETEBOOKING,
+              payload: b._id.toString(),
+            });
+          }}
+        >
+          delete booking
+        </button>
+        <button
+          onClick={() => {
+            dispatch({
+              type: ActionType.UPDATEBOOKING,
+              payload: b._id.toString(),
+            });
+          }}
+        >
+          update booking
+        </button>
       </div>
     );
   });
 
-  return (
-    // <div>
-    //   {state.map((b) => {
-    //     return (
-    //       <div key={b._id}>
-    //         <p>date: {b.date}</p>
-    //         <p>guests: {b.people}</p>
-    //         {/* <p>Firstname: {b.guest.name}</p> */}
-    //         <p>Lastname: {b.guest.lastname}</p>
-    //         <p>Phone: {b.guest.phone}</p>
-    //         <p>Email: {b.guest.email}</p>
-    //         <button onClick={deleteBooking}>delete booking</button>
-    //         <button onClick={updateBooking}>update booking</button>
-    //       </div>
-    //     );
-    //   })}
-    // </div>
-    <>{allBookings}</>
-  );
+  return <>{allBookings}</>;
 };
